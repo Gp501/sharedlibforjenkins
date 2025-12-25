97bb4c8cb41f3d167ed5106c26973859df3c268e
Gaurav <gp@gmail.com>
2025-12-25 14:38:13 +0530

    pudated grovy files

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,12 @@
 // Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
+/*def call(String ProjectName, String ImageTag, String DockerHubUser){
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
+*/
+
+// vars/docker_build.groovy
+def call(String ProjectName, String ImageTag, String acrServer){
+  // Tags the image with the full ACR path: registry.azurecr.io/image:tag
+  sh "docker build -t ${acrServer}/${ProjectName}:${ImageTag} ."
+}
+
--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,17 @@
-def call(String Project, String ImageTag, String dockerhubuser){
+/*def call(String Project, String ImageTag, String dockerhubuser){
   withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
       sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
   }
   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
+*/
+
+// vars/docker_push.groovy
+def call(String Project, String ImageTag, String acrServer){
+  // Use the Credential ID created in Jenkins for Azure ACR
+  withCredentials([usernamePassword(credentialsId: 'azure-acr-creds', passwordVariable: 'acrPass', usernameVariable: 'acrUser')]) {
+      sh "docker login ${acrServer} -u ${acrUser} -p ${acrPass}"
+  }
+  sh "docker push ${acrServer}/${Project}:${ImageTag}"
+}
+
